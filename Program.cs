0ebba7c7f691ebc9afc060b0d47db899a8427f39
David Wager <maddavid123@gmail.com>
2021-02-03 21:51:34 +0000

    Real Initial Commit
    Added Base chatbot functionality and a Verlet Swing module.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,194 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
 
-namespace MaDDTwitchBotDotNet
+namespace MaDDTwitchBot
 {
-    class Program
+    /// <summary>
+    /// Opens a Connection to Twitch and creates some base utility for command handling.
+    /// </summary>
+    internal class Program
     {
+        /// <summary>
+        /// The writer to the stream we're operating on.
+        /// Write commands go straight to twitch.
+        /// </summary>
+        private static StreamWriter writer;
+
+        /// <summary>
+        /// The reader of the stream we're operating on.
+        /// Read messages are coming from twitch.
+        /// </summary>
+        private static StreamReader reader;
+
+        /// <summary>
+        /// The channel our client is watching. This should be all lower case.
+        /// </summary>
+        private static string channel;
+
+        /// <summary>
+        /// Send a Message to the twitch IRC server useing the StreamWriter, encase the message in the ccorrect format and flush afterwards.
+        /// </summary>
+        /// <param name="message">The message to be sent to Twitch.</param>
+        /// <exception cref="ArgumentException">Thrown if message is null or empty.</exception>
+        /// <exception cref="InvalidOperationException">Thrown if the reader or writer have not yet been assinged values.</exception>
+        private static void SendMessage(string message)
+        {
+            // Do some basic null checks.
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                throw new ArgumentException($"'{nameof(message)}' cannot be null or whitespace", nameof(message));
+            }
+            if (writer == null)
+            {
+                throw new InvalidOperationException($"'{nameof(writer)}' cannot be null, ensure the TCPStream has been opened.");
+            }
+            if (reader == null)
+            {
+                throw new InvalidOperationException($"'{nameof(reader)}' cannot be null, ensure the TCPStream has been opened.");
+            }
+
+            // Messages sent to twitch should be as follows PRIVMSG #channelname : message
+            // For example PRIVMSG #maddavid123 : This is a Test Message!
+            // They should then be ended with a carriage return \r\n and flushed.
+            writer.WriteLine("PRIVMSG #" + channel + " :" + message + "\r\n");
+            writer.Flush();
+        }
+
+        /// <summary>
+        /// Process a incoming string coming from twitch.
+        /// </summary>
+        /// <param name="messageFromTwitch">The raw message we've recieved from twitch</param>
+        private static void processTwitchBackMessage(string messageFromTwitch)
+        {
+            // Every now and then, twitch will PING the client to see if it's still connected.
+            // Ensure that when they do, we return the gesture.
+            if (messageFromTwitch.Equals("PING :tmi.twitch.tv"))
+            {
+                writer.WriteLine("PONG :tmi.twitch.tv\r\n");
+                writer.Flush();
+                Console.WriteLine("Ponging twitch's ping.");
+                return; // If we do get a ping, don't try and process any further.
+            }
+
+            // Next, split the returned message from twitch into username and message.
+            // Returned message typically looks like this:
+            // :maddavid123!maddavid123@maddavid123.tmi.twitch.tv PRIVMSG #maddavid123 :!verlet10
+
+            // We can get the username and message by splitting on the colons (:) and taking the split[1] and split[2].
+            // Example:
+            // 0 -
+            // 1 - maddavid123!maddavid123@maddavid123.tmi.twitch.tv PRIVMSG #maddavid123 
+            // 2 - !verlet10
+            string[] messageSplit = messageFromTwitch.Split(':');
+
+            // Further split on split[1] on the ! symbol to get the username
+            // Example
+            // 0 - maddavid123 (We want this one!)
+            // 1 - !maddavid123@maddavid123.tmi.twitch.tv PRIVMSG #maddavid123
+            string username = messageSplit[1].Split('!')[0];
+            string message = messageSplit[2];
+            
+            //Then send it off for extra processing!
+            handleCommand(username, message);
+        }
+
+        /// <summary>
+        /// Process a chat message in a usable form.
+        /// </summary>
+        /// <param name="username">Currently unused, but for future operations we can match on a modlist or VIP list.</param>
+        /// <param name="message">The message to process.</param>
+        private static void handleCommand(string username, string message)
+        {
+            // Not sure whether this is particularly desired, but we can keep this in
+            Console.WriteLine(username + ":" + message);
+
+            // Check it's a command.
+            if (message.StartsWith('!'))
+            {
+                string command = message.Split(' ')[0]; // Commands typically have a space after them, we can just take the first part.
+                string trueCommand = command.Substring(1, command.Length-1); // Drop the first character, and the final one. This will leave us with the whole command.
+
+                // Check whether this is a verlet command.
+                if (trueCommand.StartsWith("verlet", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Remove the "verlet" part of the command, leaving you only with the level number.
+                    // Then, parse the number into an integer. Finally, send the Level, level Name and time off to twitch!
+                    string levelNumberString = trueCommand.Remove(0, 6);
+                    if (int.TryParse(levelNumberString, out int levelNumber))
+                    {
+                        SendMessage("Level " + levelNumber + " - " + VerletHighScoresParser.levelNames[levelNumber] + ": " + VerletHighScoresParser.levelTimes[levelNumber] + "s!");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets up the TCP connection to twitch, and then begins monitoring the connection for back and forth messages
+        /// </summary>
+        /// <param name="args"> args[0] should be the OAuth token for a given bot account. args[1] should be the username for the bot. args[2] is the channel we're operating in.</param>
         static void Main(string[] args)
         {
+            // Set up the HighScore parser, then grab the twitch user information from the incoming command line arguments.
+            VerletHighScoresParser.Init();
+            string pass = args[0];
+            string nick = args[1].ToLower();
+            channel = args[2].ToLower();
+
+            // Set up a TCP connection to twitch, and set up a writer/reader for the underlying stream.
+            TcpClient client = new TcpClient("irc.twitch.tv", 6667);
+            writer = new StreamWriter(client.GetStream());
+            reader = new StreamReader(client.GetStream());
+
+            // Connect to the IRC channel of the user. 
+            // First, send PASS followed by the bots's OAuth token.
+            // Secondly the NICK (The bots username)
+            // Finally JOIN #channelName where channelName is the channel we're joining.
+            writer.WriteLine("PASS " + pass + "\r\n");
+            writer.Flush();
+            writer.WriteLine("NICK " + nick + "\r\n");
+            writer.Flush();
+            writer.WriteLine("JOIN #" + channel + "\r\n");
+            writer.Flush();
+
+            // After sending that off, twitch will attempt to join the channel.
+            // Several messages are sent from twitch that ensure we're reading and processing the stream correctly.
+            // Once we've seen: End of /NAMES list we can begin operating normally.
+            bool loading = false;
+            string inBuffer = "";
+            while(!loading)
+            {
+                try
+                {
+                    inBuffer += reader.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    // HACKHACK - Exception e is way too broad and is just a catch all. Specify a better exception set.
+                    // BUGBUG - If for any reason the TCPClient connecting to that host on that port is already open it'll spam the console to all hell.
+                    Console.WriteLine(e.Message);
+                }
+                string[] lines = inBuffer.Split("\n");
+                foreach(string s in lines)
+                {
+                    loading = s.Contains("End of /NAMES list");
+                }
+            }
+
+            // Now that we've connected, we may as well let the twitch chat know we've arrived.
+            // Then keep reading lines in from Twitch and process them.
+            SendMessage("Twitch Bot: Online");
+            bool connectionOpen = true;
+            while (connectionOpen)
+            {
+                if (reader.BaseStream.CanRead)
+                {
+                    while ((inBuffer = reader.ReadLine()) != null)
+                    {
+                        processTwitchBackMessage(inBuffer);
+                    }
+                }
+            }
         }
     }
 }
---
+++ VerletHighScoresParser.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace MaDDTwitchBot
+{
+    /// <summary>
+    /// Reads an XML File containing the times that the local user has in the game Verlet Swing, and updates every time that file is updated.
+    /// </summary>
+    internal class VerletHighScoresParser
+    {
+        /// <summary>
+        /// A List containing the times for each level in the game.
+        /// </summary>
+        public static readonly List<double> levelTimes = new List<double>();
+
+        /// <summary>
+        /// A list containing the name of each level in the game.
+        /// </summary>
+        public static readonly List<string> levelNames = new List<string>();
+
+        /// <summary>
+        /// Sets up a File Watcher to fire events whenever a certain file is changed. In this case highscore.xml.
+        /// </summary>
+        private static readonly FileSystemWatcher watcher = new FileSystemWatcher();
+
+        /// <summary>
+        /// The File Location of the Highscore.xml file. 
+        /// Typically: C:\Users\CurrentUser\%appdata%\LocalLow\Flamebait Games\Verlet Swing
+        /// </summary>
+        public static string fileLocation = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"Low\Flamebait Games\Verlet Swing";
+
+        /// <summary>
+        /// Initiate the File Parser, and set up this utility class.
+        /// </summary>
+        public static void Init()
+        {
+            // Add unimportant data to element 0.
+            // Then add the name of every level to the levelNames List and run our first parse of the highscore.xml file.
+            levelNames.Add(string.Empty);
+            populateLevelNameList();
+            ReadScores();
+
+            // Set up the FileWatcher to trigger every time highscore.xml is updated.
+            watcher.Path = fileLocation;
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "highscore.xml";
+            watcher.Changed += OnHighScoreFileUpdate;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        /// <summary>
+        /// Populate the levelNames list using a hub function. The following functions are all probably optimised together.
+        /// But I believe this to be more slightly more organised.
+        /// </summary>
+        private static void populateLevelNameList()
+        {
+            populateCheckmateHistory();
+            populateMobymart();
+            populateNimisNonLaute();
+            populateWondercon1998();
+            populateCrimsonCourt();
+        }
+
+        /// <summary>
+        /// Populate the levelNames list with the names in World 1: Checkmate History.
+        /// </summary>
+        private static void populateCheckmateHistory()
+        {
+            levelNames.Add("Swing");
+            levelNames.Add("Air");
+            levelNames.Add("Tutanchess");
+            levelNames.Add("Roman Vanilla");
+            levelNames.Add("Canyon");
+            levelNames.Add("Rocket Town");
+            levelNames.Add("Pizza Dreams");
+            levelNames.Add("Around The Block");
+            levelNames.Add("Mausoleum");
+            levelNames.Add("Attack on Pillar");
+            levelNames.Add("Time Freezer");
+            levelNames.Add("Horizontality");
+            levelNames.Add("Ancient Ben");
+            levelNames.Add("Pillarloops");
+            levelNames.Add("Dynamic Discs");
+            levelNames.Add("Needles");
+            levelNames.Add("Sidewinder");
+            levelNames.Add("Easter Bridge");
+            levelNames.Add("Lefty Right");
+            levelNames.Add("Maze Runner");
+        }
+
+        /// <summary>
+        /// Populate the levelNames list with the names in World 2: Mobymart.
+        /// </summary>
+        private static void populateMobymart()
+        {
+            levelNames.Add("Bubbles");
+            levelNames.Add("Dolphin Plumber");
+            levelNames.Add("Space Buoys");
+            levelNames.Add("Ice Hoops");
+            levelNames.Add("Through The Weeds");
+            levelNames.Add("Bubble Trouble");
+            levelNames.Add("Tunnel");
+            levelNames.Add("Two Towers");
+            levelNames.Add("Rafts");
+            levelNames.Add("Elevator");
+            levelNames.Add("High Ground");
+            levelNames.Add("Upstream");
+            levelNames.Add("Gear Up");
+            levelNames.Add("Time Trial");
+            levelNames.Add("Foot Corals");
+            levelNames.Add("Shifters");
+            levelNames.Add("Dank Dolphins");
+            levelNames.Add("Clockwork");
+            levelNames.Add("Tick Tock Clock");
+            levelNames.Add("Tunnel II");
+        }
+
+        /// <summary>
+        /// Populate the levelNames list with the names in World 3: Nimis Non Laute
+        /// </summary>
+        private static void populateNimisNonLaute()
+        {
+            levelNames.Add("Face It");
+            levelNames.Add("Table Flippers");
+            levelNames.Add("Burger Flippers");
+            levelNames.Add("Meaty Slingers");
+            levelNames.Add("Neatballs");
+            levelNames.Add("Meaty Plates");
+            levelNames.Add("Jumpy Jumpy Pizza");
+            levelNames.Add("Sharp Cheddar");
+            levelNames.Add("Catch A Ride!");
+            levelNames.Add("Cream Sauce");
+            levelNames.Add("French Hotdog");
+            levelNames.Add("Burger Tower");
+            levelNames.Add("Pizza Tunnel");
+            levelNames.Add("Rocketballs");
+            levelNames.Add("Mount Yuumy");
+            levelNames.Add("Surf 'n Turf");
+            levelNames.Add("Hamburglar");
+            levelNames.Add("Pizza Valley");
+            levelNames.Add("Keep Your Head Down");
+            levelNames.Add("Waste Disposal");
+        }
+
+        /// <summary>
+        /// Populate the levelNames list with the names in World 4: Wondercon 1998
+        /// </summary>
+        private static void populateWondercon1998()
+        {
+            levelNames.Add("Lefties");
+            levelNames.Add("Truth");
+            levelNames.Add("Memory Access");
+            levelNames.Add("Render Distance");
+            levelNames.Add("Rage Quit");
+            levelNames.Add("Spears");
+            levelNames.Add("Data Mining");
+            levelNames.Add("Aroud the Corner");
+            levelNames.Add("EJECT");
+            levelNames.Add("Verticality");
+            levelNames.Add("Spears II");
+            levelNames.Add("Singularity");
+            levelNames.Add("Quick Maths");
+            levelNames.Add("But can you reach this");
+            levelNames.Add("Boxings");
+            levelNames.Add("pillartower");
+            levelNames.Add("Enter Damnation!");
+            levelNames.Add("Ziggin' 'n' a Zaggin'");
+            levelNames.Add("Spiraling Away");
+            levelNames.Add("Tunnel III");
+        }
+
+        /// <summary>
+        /// Populate the levelNames list with the names in World 5: Crimson Court
+        /// </summary>
+        private static void populateCrimsonCourt()
+        {
+            levelNames.Add("Bare Bones");
+            levelNames.Add("The Missing Link");
+            levelNames.Add("Collapse");
+            levelNames.Add("Eye of the Beast");
+            levelNames.Add("Kingdom");
+            levelNames.Add("Sticks and a Hole");
+            levelNames.Add("Thorns");
+            levelNames.Add("Spears III");
+            levelNames.Add("CROWND");
+            levelNames.Add("The Depths");
+            levelNames.Add("The Climb");
+            levelNames.Add("Rock Skipping");
+            levelNames.Add("Julbock");
+            levelNames.Add("Crevice");
+            levelNames.Add("Doom");
+            levelNames.Add("Rage");
+            levelNames.Add("Spiraling to the End");
+            levelNames.Add("Dante's Slope");
+            levelNames.Add("Elevator II");
+            levelNames.Add("Sorry");
+        }
+
+        /// <summary>
+        /// Use LINQ to XML to read the highscore.xml file and grab the times from the file.
+        /// </summary>
+        private static void ReadScores()
+        {
+            XElement scoreList = XElement.Load(fileLocation + @"\highscore.xml");
+            // TODO: LINQ here can be simplified/optimised.
+            var highScoreList = from highscoreList in scoreList.Elements() select highscoreList;
+            var highscores = from highscore in highScoreList.Elements() select highscore;
+            foreach (var s in highscores)
+            {
+                // Potential BUGBUG - Setup a tryParse here instead of a standard parse. Potential for errors.
+                levelTimes.Add(double.Parse(s.Attribute("time").Value) / 1000);
+            }
+        }
+
+        /// <summary>
+        /// Function callback for when the Highscore.xml file is changed.
+        /// </summary>
+        /// <param name="source">Unused</param>
+        /// <param name="e">Unused.</param>
+        private static void OnHighScoreFileUpdate(object source, FileSystemEventArgs e)
+        {
+            ReadScores();
+        }
+
+    }
+}
